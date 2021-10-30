4d3015ed514b56acde3bd224dc6e5ed6b696ba6f
dong99u <86235780+dong99u@users.noreply.github.com>
2021-10-30 23:00:12 +0900

    Really finish!!!
    Done!

--- js/background.js
+++ js/background.js
@@ -1,24 +1,6 @@
-/*
 const imageWidth = 1920;
 const imageHeight = 1080;
-const collectionID = 8469893; // Warm Toned Mediterranean(106 photos)
-fetch(
-  `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/`
-)
-  .then(response => {
-    const jsBody = document.querySelector('body');
-    jsBody.style = `background-image: url('${response.url}')`;
-  })
-  .catch(err => {
-    console.log(err);
-  });
-*/
-
-
-
-const imageWidth = 1920;
-const imageHeight = 1080;
-const ACCESS_KEY = "uEGPiaVNgAW9UpC1ULOV9tuBL-Mr9s51rpctuwKtbeg";
+const ACCESS_KEY = "gqbyXgfqTOQTG00mMa4zB5yTflJs7hOJeEdyfb2JKLM";
 const url = `https://api.unsplash.com/photos/random/?client_id=${ACCESS_KEY}&query=landscape`;
 
 fetch(url).then(response => response.json())
--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,81 @@
-const todoButton = document.querySelector(".fotter__container:nth-child(3) span");
+const todoButton = document.querySelector(".footer__container:nth-child(3) > span");
+const todoListForm = document.querySelector(".footer__todo footer form");
+const todoListInput = document.querySelector(".footer__todo footer form input");
+const todoList = document.querySelector(".footer__todo-list");
 
+let toDoArr = [];
 
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(toDoArr));
+}
 
-todoButton("click", handleTodoList);
+function showTodoPart() {
+    const todoList = document.querySelector(".footer__todo");
+    todoList.classList.toggle("act");
+}
+
+function handleTodoSubmit(event) {
+    event.preventDefault();
+    const newTodo = todoListInput.value;
+    todoListInput.value = "";
+    const newToDoObj = {
+        text: newTodo,
+        ID: Date.now(),
+    };
+
+    toDoArr.push(newToDoObj);
+    if (toDoArr.length <= 4) {
+        paintTodo(newToDoObj);
+        saveTodos();
+
+    }
+}
+function paintTodo(newToDoObj) {
+    const li = document.createElement("li");
+    li.id = newToDoObj.ID;
+    const todoContent = document.createElement("span");
+    todoContent.innerText = newToDoObj.text;
+    const deleteButton = document.createElement("span");
+    deleteButton.innerHTML = '<i class="fas fa-trash-alt fa-sm"></i>';
+    li.appendChild(todoContent);
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener("click", deleteTodo);
+    
+    // todo의 속성을 설정하는 함수.
+    settingTodoCSS(deleteButton, todoContent, li);
+
+    todoList.appendChild(li);
+}
+
+function deleteTodo(event) {
+    const li = event.target.parentElement.parentElement;
+    li.remove();
+    toDoArr = toDoArr.filter(toDo => toDo.ID !== parseInt(li.id));
+    saveTodos();
+}
+
+function settingTodoCSS(deleteButton, todoContent, li) {
+    deleteButton.style.fontSize = "1px";
+    deleteButton.style.cursor = "pointer"
+    todoContent.style.fontWeight = "600";
+    li.style.display = "flex";
+    li.style.justifyContent = "space-between";
+    li.style.alignItems = "center";
+    li.style.padding = "3px 0px";
+    todoContent.style.display = "block";
+    deleteButton.style.display = "block";
+    todoContent.style.width = "90%";
+}
+
+todoButton.addEventListener("click", showTodoPart);
+todoListForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodos = localStorage.getItem("todos");
+
+if (savedTodos) {
+    const parsedToDos = JSON.parse(savedTodos);
+    toDoArr = parsedToDos;
+    parsedToDos.forEach(element => {
+        paintTodo(element);
+    });
+}
